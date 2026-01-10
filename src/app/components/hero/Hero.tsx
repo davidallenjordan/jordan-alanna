a4c1c170cf565e59fc54410f61ddb4e0b375ec6f
David Allen-Jordan <dallen-jordan@nascentdigital.com>
2026-01-10 16:14:45 +1100

    feat: hero clean up

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -3,30 +3,30 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-    <div className="flex justify-center items-center bg-accent py-[60px] container">
+    <div className="flex justify-center items-center bg-accent pt-[60px] container">
       <div className="text-pearl flex flex-col items-center justify-center space-y-6 md:space-y-10 max-w-full">
-        <h1 className="flex flex-col xs:flex-row items-center justify-center gap-3 sm:gap-6 heading-1">
+        <h1 className="flex flex-row items-center justify-center gap-3 sm:gap-6 heading-1">
           <div className="flex flex-col items-end leading-none">
             <span>Jordan</span>
-            <span className="font-snell text-2xl sm:text-3xl md:text-4xl font-black mt-0">May</span>
+            <span className="font-snell text-[20px] sm:text-3xl md:text-4xl font-black mt-0">May</span>
           </div>
 
-          <span className="font-snell text-2xl sm:text-3xl md:text-4xl font-black self-center">&</span>
+          <span className="font-snell text-[20px] sm:text-3xl md:text-4xl font-black self-center">&</span>
 
           <div className="flex flex-col items-end leading-none">
             <span>Alanna</span>
-            <span className="font-snell font-black text-2xl sm:text-3xl md:text-4xl mt-0">Dunlop</span>
+            <span className="font-snell font-black text-[20px] sm:text-3xl md:text-4xl mt-0">Dunlop</span>
           </div>
         </h1>
 
-        <div className="relative w-full max-w-[650px]">
+        <div className="relative w-full max-w-[650px] pb-[120px]">
           <Image
             className="w-full h-auto"
             src={heroImage}
             alt="A series of photobooth portraits of the wedding couple"
           />
 
-          <span className="absolute bottom-2 right-0 sm:bottom-4 sm:right-2 md:bottom-0 md:right-16 font-snell text-3xl xs:text-5xl md:text-7xl rotate-[-8deg] origin-bottom-right translate-x-0 translate-y-0 md:translate-x-[20%] md:translate-y-[20%] whitespace-nowrap">
+          <span className="font-snell text-[2rem] xs:text-[3.5rem] sm:text-[4.5rem] md:text-[5rem] absolute bottom-16 right-0 left-0 text-center rotate-[-8deg]">
             are getting married
           </span>
         </div>
