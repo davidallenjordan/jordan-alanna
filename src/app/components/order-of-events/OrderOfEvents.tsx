ee520e1d31a7cc0931601eb84691f2b3c580755f
David Allen-Jordan <dallen-jordan@nascentdigital.com>
2026-01-25 18:04:05 -0500

    feat: adding divider to order of events

--- src/app/components/order-of-events/OrderOfEvents.tsx
+++ src/app/components/order-of-events/OrderOfEvents.tsx
@@ -7,22 +7,26 @@ export default function OrderOfEvents() {
       <div className="container py-[60px] text-accent">
         <h2 className="heading-2 pb-[16px] sm:pb-[32px] text-center">Order of Events</h2>
 
-        <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0">
+        <ul className="flex flex-col md:flex-row">
           <li className="md:w-1/5 flex flex-col items-center text-center">
             <span className="font-didot text-[32px]">4:00 PM</span>
             <span className="font-didot text-[32px]">Arrivals</span>
+            <span className="md:hidden font-didot text-[32px]">—</span>
           </li>
           <li className="md:w-1/5 flex flex-col items-center text-center">
             <span className="font-didot text-[32px]">4:30 PM</span>
             <span className="font-didot text-[32px]">Ceremony</span>
+            <span className="md:hidden font-didot text-[32px]">—</span>
           </li>
           <li className="md:w-1/5 flex flex-col items-center text-center">
             <span className="font-didot text-[32px]">5:00 PM</span>
             <span className="font-didot text-[32px]">Cocktail Hour</span>
+            <span className="md:hidden font-didot text-[32px]">—</span>
           </li>
           <li className="md:w-1/5 flex flex-col items-center text-center">
             <span className="font-didot text-[32px]">6:30 PM</span>
             <span className="font-didot text-[32px]">Dinner</span>
+            <span className="md:hidden font-didot text-[32px]">—</span>
           </li>
           <li className="md:w-1/5 flex flex-col items-center text-center">
             <span className="font-didot text-[32px] my-auto">Dancing - late!</span>
