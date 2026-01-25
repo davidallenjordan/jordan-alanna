329c2d646576a6c0eb2beaa5c86d235463941fbe
David Allen-Jordan <dallen-jordan@nascentdigital.com>
2026-01-25 17:17:06 -0500

    feat: hero tweaks

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -8,14 +8,14 @@ export default function Hero() {
         <h1 className="flex flex-row items-center justify-center gap-3 sm:gap-6 heading-1 uppercase">
           <div className="flex flex-col items-end leading-none">
             <span>Jordan</span>
-            <span className="font-snell text-[20px] sm:text-3xl md:text-[80px] font-black mt-0 normal-case">May</span>
+            <span className="font-snell text-[20px] sm:text-3xl md:text-[80px] font-black -mt-3 normal-case">May</span>
           </div>
 
           <span className="font-snell text-[20px] sm:text-3xl md:text-[80px] font-black self-center">&</span>
 
           <div className="flex flex-col items-end leading-none">
             <span>Alanna</span>
-            <span className="font-snell font-black text-[20px] sm:text-3xl md:text-[80px] mt-0 normal-case">Dunlop</span>
+            <span className="font-snell font-black text-[20px] sm:text-3xl md:text-[80px] -mt-3 normal-case">Dunlop</span>
           </div>
         </h1>
 
